e27c765a3dc87834c936aec57138117660775ebe
Ilya Sanakoev <vizhu_stasa@mail.ru>
2020-11-28 03:43:28 +0300

    working script, need to restyle, refactor and optimize it

--- abyss.py
+++ abyss.py
@@ -1,5 +1,6 @@
 import time
 import json
+import collections
 
 import requests
 
@@ -13,13 +14,13 @@ def request_one_page(username, page_number):
         'method': 'user.getrecenttracks',
         'api_key': API_KEY,
         'format': 'json',
-        'limit': '100',
+        'limit': '10',
         'page': page_number
     }
     r = requests.get(BASE_API_LINK, params=params)
     data = json.loads(r.text).get('recenttracks')
     # page = json.dumps(a, indent=4, ensure_ascii=False)
-    print(data)
+    # print(data)
     return data
 
 
@@ -30,9 +31,30 @@ prev_track = response.get('track')[0].get('artist').get('#text') + ' - ' + \
              response.get('track')[0].get('name')
 print('Most recent track:', prev_track)
 print('Total pages to parse:', total_pages)
-for page in range(1, total_pages+1):
+counter = collections.Counter()
+print(counter)
+a = 0
+for page in range(1, 6):
     current_page = request_one_page(username, page).get('track')
     print('Current page:', page)
-    # for track in current_page:
-    #     print('some parsing')
+    for track in current_page:
+        if track.get('@attr'):
+            print('Please close your music player')
+        else:
+            curr_track = track.get('artist').get('#text') + ' - ' + \
+                         track.get('name')
+            if prev_track != curr_track:
+                prev_track = curr_track
+                if counter[curr_track]:
+                    for i in range(1, 10000000):
+                        if counter[curr_track + ' (another portion ' + str(i) + ')']:
+                            pass
+                        else:
+                            a = i
+                            break
+            if a == 0:
+                counter[curr_track] += 1
+            else:
+                counter[curr_track + ' (another portion ' + str(a) + ')'] += 1
     time.sleep(0.25)
+print(counter.most_common(10))
